3ae665ab3303007dca768814900ffacfc13a00e2
marcocacciabue <64919062+marcocacciabue@users.noreply.github.com>
2024-02-19 11:54:38 -0300

    working on english version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-                      
+let lang = navigator.language.slice(0, 2); // Get the first two letters of user's language preference
+
+// Hide elements that don't correspond to the user's preferred language
+if (lang === 'en') {
+  document.querySelectorAll('.second-language').forEach(elem => elem.style.display = 'none');
+} else {
+  document.querySelectorAll('.english').forEach(elem => elem.style.display = 'none');
+}                      
 
 (function ($) { "use strict";
 	
